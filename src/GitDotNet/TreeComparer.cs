fffb48870e46711259ca8a5f50d98975c09cf0de
frblondin <thomas@caudal.fr>
2025-04-13 17:46:36 +0200

    Replace levenshtein similarity with line-level heuristic (much faster)

--- src/GitDotNet/TreeComparer.cs
+++ src/GitDotNet/TreeComparer.cs
@@ -1,7 +1,9 @@
-using Microsoft.Extensions.Options;
+using System.Threading.Channels;
 using GitDotNet.Tools;
-using BlobEntryPath = (GitDotNet.GitPath Path, GitDotNet.TreeEntryItem Blob);
+using Microsoft.Extensions.Options;
 using AddedOrRemovedBlobEntryPath = (bool IsNew, (GitDotNet.GitPath Path, GitDotNet.TreeEntryItem Blob) Element);
+using BlobEntryPath = (GitDotNet.GitPath Path, GitDotNet.TreeEntryItem Blob);
+using System.Linq;
 
 namespace GitDotNet;
 
@@ -11,14 +13,49 @@ internal class TreeComparer(IOptions<GitConnection.Options> options) : ITreeComp
     {
         var result = new List<Change>();
 
-        var oldBlobs = old is not null ? old.GetAllBlobEntriesAsync().ToEnumerable().ToList() : null;
-        var newBlobs = @new is not null ? @new.GetAllBlobEntriesAsync().ToEnumerable().ToList() : null;
+        var oldBlobs = old is not null ? GetDivergingBlobEntries(old, @new).ToEnumerable().ToList() : null;
+        var newBlobs = @new is not null ? GetDivergingBlobEntries(@new, old).ToEnumerable().ToList() : null;
 
         FindModifiedBlobsUsingIds(result, oldBlobs, newBlobs);
         FindRenamedBlobsWithSameId(result, oldBlobs, newBlobs);
         await FindRenamedBlobsWithSimilarity(options, result);
 
         return [.. result.OrderBy(c => c.NewPath ?? c.OldPath)];
+    }
+
+    private static async IAsyncEnumerable<BlobEntryPath> GetDivergingBlobEntries(TreeEntry tree, TreeEntry? other)
+    {
+        var channel = Channel.CreateUnbounded<BlobEntryPath>();
+        var task = GetDivergingBlobEntries(channel, tree, other, new Stack<string>());
+        await foreach (var result in channel.Reader.ReadAllAsync())
+        {
+            yield return result;
+        }
+        await task;
+    }
+
+    private static async Task GetDivergingBlobEntries(Channel<BlobEntryPath> channel, TreeEntry tree, TreeEntry? other, Stack<string> path)
+    {
+        foreach (var item in tree.Children.Except(other?.Children ?? []))
+        {
+            path.Push(item.Name);
+            if (item.Mode.Type == ObjectType.Tree)
+            {
+                var child = await item.GetEntryAsync<TreeEntry>();
+
+                // Look for tree with same name, meaning that a nested object has been modified
+                var otherItem = other?.Children.FirstOrDefault(t => t.Name == item.Name && t.Mode.Type == ObjectType.Tree);
+                var otherChild = otherItem is not null ? await otherItem.GetEntryAsync<TreeEntry>() : null;
+
+                await GetDivergingBlobEntries(channel, child, otherChild, path);
+            }
+            if (item.Mode.Type == ObjectType.RegularFile)
+            {
+                await channel.Writer.WriteAsync((path.Reverse().ToArray(), item));
+            }
+            path.Pop();
+        }
+        if (path.Count == 0) channel.Writer.Complete();
     }
 
     private static void FindModifiedBlobsUsingIds(List<Change> result,
@@ -117,13 +154,15 @@ internal class TreeComparer(IOptions<GitConnection.Options> options) : ITreeComp
     private static async Task FindRenamedBlobsWithSimilarity(IOptions<GitConnection.Options> options, List<Change> result)
     {
         var addedItems = result.Where(c => c.Type == ChangeType.Added).ToList();
-        foreach (var removedItem in result.Where(c => c.Type == ChangeType.Removed).ToList())
+        var removedItems = result.Where(c => c.Type == ChangeType.Removed).ToList();
+        var chunkCache = new Dictionary<BlobEntry, HashSet<int>>();
+        foreach (var removedItem in removedItems)
         {
-            await FindSimilarRenamedBlobs(options, result, addedItems, removedItem);
+            await FindSimilarRenamedBlobs(options, result, addedItems, removedItem, chunkCache);
         }
     }
 
-    private static async Task FindSimilarRenamedBlobs(IOptions<GitConnection.Options> options, List<Change> result, List<Change> addedItems, Change removedItem)
+    private static async Task FindSimilarRenamedBlobs(IOptions<GitConnection.Options> options, List<Change> result, List<Change> addedItems, Change removedItem, IDictionary<BlobEntry, HashSet<int>> chunkCache)
     {
         foreach (var addedItem in addedItems)
         {
@@ -147,28 +186,89 @@ internal class TreeComparer(IOptions<GitConnection.Options> options) : ITreeComp
                 continue;
             }
 
-            var flowControl = EvaluateRenameSimilarity(options, result, addedItems, removedItem, addedItem, removedEntry, addedEntry);
+            var flowControl = EvaluateRenameSimilarity(addedItem, removedEntry, addedEntry);
             if (!flowControl)
             {
                 break;
             }
         }
+
+        bool EvaluateRenameSimilarity(Change addedItem, BlobEntry removedEntry, BlobEntry addedEntry)
+        {
+            var similarity = EvaluateBlobSimilarity(removedEntry, addedEntry, chunkCache);
+            if (similarity >= options.Value.RenameThreshold)
+            {
+                var renameChange = new Change(ChangeType.Renamed, removedItem.OldPath, addedItem.NewPath, removedItem.Old, addedItem.New);
+                result.Remove(removedItem);
+                result.Remove(addedItem);
+                result.Add(renameChange);
+                addedItems.Remove(addedItem);
+                return false;
+            }
+
+            return true;
+        }
     }
 
-    private static bool EvaluateRenameSimilarity(IOptions<GitConnection.Options> options, List<Change> result, List<Change> addedItems, Change removedItem, Change addedItem, BlobEntry removedEntry, BlobEntry addedEntry)
+    private static float EvaluateBlobSimilarity(BlobEntry a, BlobEntry b, IDictionary<BlobEntry, HashSet<int>> chunkCache)
     {
-        var similarity = LevenshteinDistance.ComputeSimilarity(removedEntry.Data, addedEntry.Data, options.Value.RenameThreshold);
-        if (similarity >= options.Value.RenameThreshold)
+        var aChunks = GetChunks(a, chunkCache);
+        var bChunks = GetChunks(b, chunkCache);
+        var commonLines = CountIntersections(aChunks, bChunks);
+        var similarity = 2F * commonLines / (aChunks.Count + bChunks.Count);
+        return similarity;
+    }
+
+    private static HashSet<int> GetChunks(BlobEntry entry, IDictionary<BlobEntry, HashSet<int>> chunkCache) =>
+        chunkCache.TryGetValue(entry, out var chunks) ?
+        chunks :
+        chunkCache[entry] = SplitIntoChunkHashes(entry.Data);
+
+    private static int CountIntersections(HashSet<int> first, HashSet<int> second) =>
+        (from value in first
+         where second.Contains(value)
+         select value).Count();
+
+    private static HashSet<int> SplitIntoChunkHashes(byte[] data)
+    {
+        var result = new HashSet<int>();
+        int start = 0;
+        int lineEnding;
+        while ((lineEnding = Array.IndexOf(data, (byte)'\n', start)) != -1)
         {
-            var renameChange = new Change(ChangeType.Renamed, removedItem.OldPath, addedItem.NewPath, removedItem.Old, addedItem.New);
-            result.Remove(removedItem);
-            result.Remove(addedItem);
-            result.Add(renameChange);
-            addedItems.Remove(addedItem);
-            return false;
+            result.Add(ComputeHash(data.AsSpan(start, lineEnding - start)));
+            start = lineEnding + 1;
         }
 
-        return true;
+        // Handle the last chunk if it doesn't end with a newline
+        if (start < data.Length)
+        {
+            result.Add(ComputeHash(data.AsSpan(start)));
+        }
+        return result;
+    }
+
+    public static int ComputeHash(Span<byte> data)
+    {
+        unchecked
+        {
+            // The prime number used to compute the FNV hash.
+            const int Prime = 16777619;
+            // The starting point of the FNV hash.
+            const long Offset = 2166136261;
+
+            int hash = (int)Offset;
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (data[i] != (byte)'\r') // Ignore \r
+                {
+                    hash = (hash ^ data[i]) * Prime;
+                }
+            }
+
+            return hash;
+        }
     }
 }
 
